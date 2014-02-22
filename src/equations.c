0bf69a213208cf4dd2d29076272098ff23e3e654
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-21 21:50:45 -0500

    Working disc generation

--- src/equations.c
+++ src/equations.c
@@ -4,23 +4,46 @@ static Window *window;
 static TextLayer *text_layer;
 GBitmap *future_bitmap;
 BitmapLayer *future_layer;
+GPoint p0, p1;
+static GRect window_frame;
+static Layer *disc_layer;
 
+#define MAXWIDTH 144
+#define MAXHEIGHT 168
 
+int get_number_of_perimeter_pixels(int width, int height) {
+  return (2*width + 2*height - 4);
+}
 
-static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-
-  future_bitmap = gbitmap_create_with_resource(RESOURCE_ID_EQUATION_IMAGE);
-  future_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
-  bitmap_layer_set_bitmap(future_layer, future_bitmap);
-  bitmap_layer_set_background_color(future_layer, GColorClear);
-  bitmap_layer_set_alignment(future_layer, GAlignCenter);
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(future_layer));
-
+static void disc_draw(GContext *ctx) {
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_circle(ctx, GPoint(50, 50), 20);
 }
 
 
+static void disc_layer_update_callback(Layer *me, GContext *ctx) {
+  disc_draw(ctx);
+}
+
+static void window_load(Window *window) {
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect frame = window_frame = layer_get_frame(window_layer);
+
+  future_bitmap = gbitmap_create_with_resource(RESOURCE_ID_EQUATION_IMAGE);
+  future_layer = bitmap_layer_create(GRect(0, 0, MAXWIDTH, MAXHEIGHT));
+  bitmap_layer_set_bitmap(future_layer, future_bitmap);
+  bitmap_layer_set_background_color(future_layer, GColorClear);
+  bitmap_layer_set_alignment(future_layer, GAlignCenter);
+  layer_add_child(window_layer, bitmap_layer_get_layer(future_layer));
+
+
+  disc_layer = layer_create(frame);
+  layer_set_update_proc(disc_layer, disc_layer_update_callback);
+
+  layer_add_child(window_layer, disc_layer);
+
+}
 
 static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
