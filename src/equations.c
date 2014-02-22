ce66d735f34ee4b3606bee42b6b578eacf314f10
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-21 20:39:39 -0500

    centered

--- src/equations.c
+++ src/equations.c
@@ -12,9 +12,10 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   future_bitmap = gbitmap_create_with_resource(RESOURCE_ID_EQUATION_IMAGE);
-  future_layer = bitmap_layer_create(GRect(0, 0, 91, 100));
+  future_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(future_layer, future_bitmap);
   bitmap_layer_set_background_color(future_layer, GColorClear);
+  bitmap_layer_set_alignment(future_layer, GAlignCenter);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(future_layer));
 
 }
