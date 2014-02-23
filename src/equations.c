debbf2a86d9b548ae2606f9427da95c3efb79970
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-22 19:07:16 -0500

    got the proper hour equation working

--- src/equations.c
+++ src/equations.c
@@ -5,6 +5,7 @@ GBitmap *future_bitmap;
 BitmapLayer *future_layer;
 static GRect window_frame;
 static Layer *disc_layer;
+static Layer *equation_layer;
 
 #define MAXWIDTH 144
 #define MAXHEIGHT 168
@@ -90,7 +91,7 @@ static void disc_draw(GContext *ctx, int x, int y) {
 
 static void window_load(Window *window) {
 
-  Layer *window_layer = window_get_root_layer(window);
+  /*Layer *window_layer = window_get_root_layer(window);
   GRect frame = window_frame = layer_get_frame(window_layer);
 
   future_bitmap = gbitmap_create_with_resource(IMAGE_RESOURCE_IDS[0]);
@@ -99,11 +100,7 @@ static void window_load(Window *window) {
   bitmap_layer_set_background_color(future_layer, GColorClear);
   bitmap_layer_set_alignment(future_layer, GAlignCenter);
   layer_add_child(window_layer, bitmap_layer_get_layer(future_layer));
-
-
-  disc_layer = layer_create(frame);
-  //layer_set_update_proc(disc_layer, disc_layer_update_callback);
-  layer_add_child(window_layer, disc_layer);
+  */
 }
 
 static void window_unload(Window *window) {
@@ -114,16 +111,49 @@ void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
   //
 }
 
+void handle_hour_tick(struct tm *tick_time, TimeUnits units_changed) {
+  layer_mark_dirty(equation_layer);
+  //
+}
+
 void disc_layer_update_callback(Layer *me, GContext* ctx) {
 
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
   int minutes = t->tm_sec;
-
-  //int pixels = get_number_of_perimeter_pixels(MAXWIDTH, MAXHEIGHT); // 620 perimeter pixels
+  int hours = t->tm_hour;
 
   disc_draw(ctx, get_disc_x_position(minutes), get_disc_y_position(minutes));
 
+  gbitmap_destroy(future_bitmap);
+  bitmap_layer_destroy(future_layer);
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect frame = window_frame = layer_get_frame(window_layer);
+  
+
+  future_bitmap = gbitmap_create_with_resource(IMAGE_RESOURCE_IDS[hours%12]);
+  future_layer = bitmap_layer_create(GRect(0, 0, MAXWIDTH, MAXHEIGHT));
+  bitmap_layer_set_bitmap(future_layer, future_bitmap);
+  bitmap_layer_set_background_color(future_layer, GColorClear);
+  bitmap_layer_set_alignment(future_layer, GAlignCenter);
+  layer_add_child(window_layer, bitmap_layer_get_layer(future_layer));
+}
+
+void equation_layer_update_callback(Layer *me, GContext* ctx) {
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+  int hour = t->tm_hour;
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect frame = window_frame = layer_get_frame(window_layer);
+
+  future_bitmap = gbitmap_create_with_resource(IMAGE_RESOURCE_IDS[hour]);
+  future_layer = bitmap_layer_create(GRect(0, 0, MAXWIDTH, MAXHEIGHT));
+  bitmap_layer_set_bitmap(future_layer, future_bitmap);
+  bitmap_layer_set_background_color(future_layer, GColorClear);
+  bitmap_layer_set_alignment(future_layer, GAlignCenter);
+  layer_add_child(window_layer, bitmap_layer_get_layer(future_layer));
 }
 
 static void init(void) {
@@ -143,7 +173,12 @@ static void init(void) {
   layer_set_update_proc(disc_layer, &disc_layer_update_callback);
   layer_add_child(root_layer, disc_layer);
 
+  /*equation_layer = layer_create(frame);
+  layer_set_update_proc(equation_layer, &equation_layer_update_callback);
+  layer_add_child(root_layer, equation_layer);*/
+
   tick_timer_service_subscribe(SECOND_UNIT, &handle_second_tick);
+  //tick_timer_service_subscribe(HOUR_UNIT, &handle_hour_tick);
 }
 
 static void deinit(void) {
